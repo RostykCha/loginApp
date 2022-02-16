19ca281934bb1aba9d075b17a5f1dd1cb84da493
RostikGlob <rostyslav.chabria@globallogic.com>
2022-02-16 22:06:02 +0200

    Working user creation

--- src/main/java/com/example/loginapp/appuser/AppUserService.java
+++ src/main/java/com/example/loginapp/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +12,28 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MESSAGE = "user with email %s not found";
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE, email)));
+    }
+
+    public String signUpUSer(AppUser appUser) {
+        boolean userExists = userRepository.findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if (userExists) {
+            throw new IllegalStateException("User exists with email " + appUser.getEmail());
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        userRepository.save(appUser);
+
+        //TODO: Send confirmation token
+        return "it works";
     }
 }
